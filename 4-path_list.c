991a7d4f048f16d34850494db0973f8dd476f928
Stuart <108@holbertonschool.com>
2017-04-04 15:58:09 +0000

    update 4-path_list.c

--- 4-path_list.c
+++ 4-path_list.c
@@ -1,70 +1,12 @@
 #include "headers.h"
 
-typedef struct path_s
+typedef struct list_s
 {
-	char *path;
-	struct path_s *next;
-} path_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
-
-size_t print_list(const path_t *h)
-{
-	size_t len = 0;
-
-	while (h)
-	{
-		printf("%s\n", h->path);
-		h = h->next;
-		len++;
-	}
-	return (len);
-}
-
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-	return (c);
-}
-
-/**
- * add_node_end - adds new node at end of a list_t list.
- * @head: array of linked list_t structures
- * @str: string to add to this node
- * Return: address of added list element; NULL if failed.
- */
-
-/* str needs to be duplicated */
-/* can use strdup */
-/* return NULL if failed */
-path_t *add_node_end(path_t **head, const char *path)
-{
-	path_t *new_node = malloc(sizeof(path_t));
-	path_t *temp_node;
-
-	if (!new_node)
-		return (NULL);
-
-	new_node->next = NULL;
-
-	if (!*head)
-		*head = new_node;
-	else
-	{
-		temp_node = *head;
-		while (temp_node->next)
-			temp_node = temp_node->next;
-
-		temp_node->next = new_node;
-	}
-	return (new_node);
-}
-
-/**
- * gets a specific environment variable
- */
 char *_getenv(const char *name)
 {
 	extern char **environ;
@@ -89,33 +31,58 @@ char *_getenv(const char *name)
 	return ("unable to locate requested environmental variable\n");
 }
 
-/**
- *
- */
-int main (int argc, char **argv, char **env)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	path_t *head = NULL;
-	//head = NULL;
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *temp_node;
 
-	char *path, *token;
-	path_t *p_list = malloc(sizeof(path_t) * 8 -1);
-	path_t *temp;
-	int i = 0;
+	if (!new_node)
+		return (NULL);
 
-	char **path_array = malloc(sizeof(char) * 8);
-	path = _getenv("PATH");
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		temp_node = *head;
+		while (temp_node->next)
+			temp_node = temp_node->next;
+
+		temp_node->next = new_node;
+	}
+	return (new_node);
+}
+
+size_t print_list(const list_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next; len++;
+	}
+	return (len);
+}
+
+int  main(int argc, char *argv[])
+{
+	list_t *head = NULL;
+	char *path = _getenv("PATH");
+	char *token;
+	if (!path)
+		return (-1);
+
 	token = strtok(path, ":");
 
-	while(token = strtok(NULL, ":"))
+	while (token)
 	{
-		path_array[i] = token;
-//		printf("%s\n", path_array[i]);
-		i++;
+		add_node_end(&head, token);
+		token = strtok(NULL, ":");
 	}
-
-	add_node_end(&head, path_array[i]);
-	//print_list(head);
-
-
-	return (0);
+	print_list(head);
+    return (0);
 }
