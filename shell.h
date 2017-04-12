e23b8b8f779b2aad01675d04cfc0b8b447aac0dd
jarehec <bone1198@gmail.com>
2017-04-12 17:22:03 +0000

    moved to main dir

--- shell.h
+++ shell.h
@@ -1,9 +1,12 @@
 #ifndef SHELL_H
 #define SHELL_H
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
+#define PROMPT "$ "
 typedef struct env_s
 {
 	char *str;
