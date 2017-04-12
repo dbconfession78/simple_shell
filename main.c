9461bfdc89ba1e971e68a0988ac23c2286f3ae2e
Stuart Kuredjian <108@holbertonschool.com>
2017-04-12 03:11:00 +0000

    handle_command.c, tokenize_string.c initial commit. update main.c

--- main.c
+++ main.c
@@ -1,5 +1,17 @@
 #include "shell.h"
 
+size_t print_list(const env_t *h)
+{
+	size_t len = 0;
+
+	while (h)
+	{
+		printf("%s\n", h->str);
+		h = h->next; len++;
+	}
+	return (len);
+}
+
 /**
  * main - entry point for shell programs
  * @argc: argument count
@@ -11,12 +23,24 @@
 int main(int argc, char **argv, char **envp)
 {
 	(void) argc; (void) argv;
+	char *line = NULL;
+	char **tok_args;
 	env_t *head = NULL;
+	size_t len = 0;
+	ssize_t read;
+
 	init_env_list(envp, &head);
+	write (STDOUT_FILENO, PROMPT, _strlen(PROMPT));
+	while ((read = getline(&line, &len, stdin)) != EOF)
+	{
+		tok_args = tokenize_string(line);
+		handle_command(tok_args);
+		write(STDOUT_FILENO, PROMPT, _strlen(PROMPT));
+		free(tok_args);
+	}
+
 	print_env_list(head);
 	_setenv("lalala", "JARED", &head);
 	_setenv("PATH", "no/more/path", &head);
-	print_env_list(head);
-	_unsetenv("lalala", &head);
-	print_env_list(head);
+	print_list(head);
 }
---
+++ tokenize_string.c
@@ -0,0 +1,17 @@
+#include "shell.h"
+
+char **tokenize_string(char *line)
+{
+	char *token, *delim = " \n";
+	char **args = malloc(sizeof(char*) * 100);
+	size_t i = 0;
+
+	token = strtok(line, delim);
+	while(token != NULL)
+	{
+		args[i] = token;
+		token = strtok(NULL, delim);
+		++i;
+	}
+	return(args);
+}
