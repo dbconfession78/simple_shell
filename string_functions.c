132629d5949d82d54ee287d9de2f1d0611cd7b24
jarehec <bone1198@gmail.com>
2017-04-13 21:52:00 +0000

    added _strcat, added prototypes for init_path

--- string_functions.c
+++ string_functions.c
@@ -91,3 +91,24 @@ void _puts(char *str)
 	}
 	_putchar('\n');
 }
+/**
+* _strcat - concatenates two strings
+* @src: first string
+* @dest: second string
+* Return: dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	int len, dlen, i;
+
+	for (len = 0; src[len] != '\0'; len++)
+	;
+	for (dlen = 0; dest[dlen] != '\0'; dlen++)
+	;
+
+	for (i = 0; i < len && src[i] != '\0'; i++)
+		dest[dlen + i] = src[i];
+	dest[dlen + i] = '\0';
+
+	return (dest);
+}
